24e33389d910e3615add394748082fa2f4308008
satya <satya@workflowfusions.com>
2018-11-14 19:18:02 -0800

    updates

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
-
-app.use(express.static(__dirname, 'dist', {index: false}));
-
-
-server.listen(port, function() {
-    console.log("App running on port " + port);
-})
-
-// PathLocationStrategy
-
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
